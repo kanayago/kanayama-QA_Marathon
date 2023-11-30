277cea89c505d8c9e68ca7e23038ebbda95cb5fe
Gouki Kanayama <goki.kanayama@rakus-partners.co.jp>
2023-11-30 13:14:31 +0900

    11km　詳細画面追加、 顧客情報削除機能実装

--- src/node/index.js
+++ src/node/index.js
@@ -30,6 +30,38 @@ app.get("/customers", async (req, res) => {
   }
 });
 
+app.get("/customers/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const customerData = await pool.query("SELECT * FROM customers WHERE customer_id = $1", [customerId]);
+
+    if (customerData.rows.length > 0) {
+      res.send(customerData.rows[0]);
+    } else {
+      res.status(404).send("Customer not found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error " + err);
+  }
+});
+
+app.delete("/customers/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const deleteResult = await pool.query("DELETE FROM customers WHERE customer_id = $1 RETURNING *", [customerId]);
+
+    if (deleteResult.rows.length > 0) {
+      res.json({ success: true, deletedCustomer: deleteResult.rows[0] });
+    } else {
+      res.status(404).json({ success: false, message: "Customer not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Error deleting customer" });
+  }
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
